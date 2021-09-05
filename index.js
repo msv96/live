ad9393df3e4b2d6a2f447c4888a596aaef83b45b
msv96 <msrini1100@gmail.com>
2021-09-05 21:16:22 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
 const app = express();
-const cors = require("cors");
+const data = [];
 
 app.use(
 	cors({
@@ -8,24 +9,60 @@ app.use(
 	})
 );
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-	res.send("Hello. Welcome to the App");
+	res.send(data);
 });
 
-app.get("/users", (req, res) => {
-	res.json({ message: "All is well" });
+app.post("/signin", (req, res) => {
+	if (data == []) {
+		res.json({
+			msg: "First create a user",
+			code: false,
+		});
+	} else {
+		let idx = data.filter((e) => e.mail == req.body.mail);
+		if (idx) {
+			if (idx[0].pwd === req.body.pwd) {
+				res.json({
+					id: idx[0].id,
+					name: idx[0].name,
+					mail: idx[0].mail,
+					msg: "Log in success",
+					code: true,
+				});
+			}
+		} else {
+			res.json({
+				msg: "Username / Password is wrong",
+				code: false,
+			});
+		}
+	}
 });
 
-app.get("/products", (req, res) => {
-	res.send("<h1>All products have been sold out</h1>");
+app.post("/register", (req, res) => {
+	if (data.find((e) => e.mail === req.body.mail)) {
+		res.json({
+			msg: "User already exist",
+			code: false,
+		});
+	} else {
+		req.body.id = data.length + 1;
+		data.push(req.body);
+		res.json({
+			id: req.body.id,
+			msg: "User created successfully",
+			code: true,
+		});
+	}
 });
 
-app.get("/todolist", (req, res) => {
-	res.json([
-		{ id: 1, message: "Go to Gym", status: false },
-		{ id: 2, message: "Have Breakfast", status: false },
-		{ id: 3, message: "Go to Office", status: false },
-	]);
+app.get("/logout", (req, res) => {
+	res.json({
+		msg: "Logged out successfully",
+	});
 });
 
 app.listen(3500, () => {
